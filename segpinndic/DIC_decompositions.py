a6a348fe583e2e8a6076dfd11900aeaa4ace8286
LeeBDa <66620096+lbd-hfut@users.noreply.github.com>
2026-02-12 18:31:59 +0800

    Update DIC_decompositions.py

--- segpinndic/DIC_decompositions.py
+++ segpinndic/DIC_decompositions.py
@@ -2,5 +2,6 @@ from segpinndic.DIC_importlib import jnp, vmap, tree_map, np, plt, mcoll
 
 from segpinndic import DIC_windows
 from segpinndic import DIC_networks
+from segpinndic.utils.jax_util import tree_index
 from segpinndic.utils.other import colors
 
