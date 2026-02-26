a3633cc496571b56266865adc5e968389de547a7
LeeBDa <66620096+lbd-hfut@users.noreply.github.com>
2026-02-26 16:04:34 +0800

    Update DIC_analysis.py

--- segpinndic/DIC_analysis.py
+++ segpinndic/DIC_analysis.py
@@ -27,17 +27,11 @@ def main(
         c_ = Constants(DIC_config, roi_id)
         constants_list.append(c_)
     
-    constants_list[0].get_outdirs()
-    constants_list[0].clear_outdirs()
-    writer = SummaryWriter(constants_list[0].summary_out_dir)
-    
     if np.prod(tuple(DIC_config.n_subdomains)) == 1:
         logger.info("using PINN solver")
-        writer.add_text("solver", "using PINN solver")
         trainer = PINNTrainer
     else:
         logger.info("using FBPPINN solver")
-        writer.add_text("solver", "using FBPINN solver")
         trainer = FBPINNTrainer
     
     for i in range(N_pairs):
@@ -57,9 +51,9 @@ def main(
         
         for roi_id in range(N_roi):
             logger.info(f"Processing imgage pair {i+1}/{N_pairs} ROI {roi_id+1}/{N_roi}")
-            writer.add_text("solver", f"Processing imgage pair {i+1}/{N_pairs} ROI {roi_id+1}/{N_roi}")
             c = constants_list[roi_id]
-            run = trainer(c, writer)
+            run = trainer(c)
+            
         
             
             
