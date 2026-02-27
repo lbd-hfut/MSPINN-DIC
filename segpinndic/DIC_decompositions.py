8b7b9c95949b7f16e968e9a80889df1a9f0f4a84
LeeBDa <66620096+lbd-hfut@users.noreply.github.com>
2026-02-27 09:44:20 +0800

    Update DIC_decompositions.py

--- segpinndic/DIC_decompositions.py
+++ segpinndic/DIC_decompositions.py
@@ -150,6 +150,7 @@ class RectangularDecompositionND(Decomposition):
         mu_N = np.repeat(mu, xmins.shape[0], axis=0)
         sd_N = np.repeat(sd, xmins.shape[0], axis=0)
         unnorms = np.stack([mu_N, sd_N], axis=1)   # (N,2,2)
+        # unnorms = jnp.expand_dims(unnorms, 2) # (N,2,1,2)
 
         # get pou index
         # important note: each POU MUST cover the entire domain (POU boundary introduces discontinuities)
