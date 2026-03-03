4a24c5234977a9b64f4d04b6d26c88f2c06ce12c
LeeBDa <66620096+lbd-hfut@users.noreply.github.com>
2026-03-03 12:30:43 +0800

    Update DIC_config.py

--- segpinndic/DIC_config.py
+++ segpinndic/DIC_config.py
@@ -60,7 +60,7 @@ def seed_config_txt(path, verbose=True):
 
 
 ALL_KEYS = [
-    "input_dir", "output_dir", "run", "n_subdomains",
+    "input_dir", "output_dir", "n_subdomains",
     "hidden_units", "network", "spline_degree",
     "adam_epochs", "seed_flag", "seed_train_epochs",
     "adam_lr", "summary_freq", "test_freq", "model_save_freq",
